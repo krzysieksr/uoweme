a7bc90f8ed880ca4de9a068ee5304f22345f036f
Krzysiek <krzysztof_sroczyk@epam.com>
2018-12-12 14:07:07 +0100

    Initial commit

---
+++ src/main/java/com/dev/training/uownme/UownmeApplication.java
@@ -0,0 +1,12 @@
+package com.dev.training.uownme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UownmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UownmeApplication.class, args);
+    }
+}
