abfa4d5c64e690e95378f32c9497d2df59a719e2
Krzysiek <krzysieksr@wp.pl>
2018-12-13 11:21:23 +0100

    Corrected app main class name and added project lombok to dependences.

--- src/main/java/com/dev/training/uownme/UowemeApplication.java
+++ src/main/java/com/dev/training/uownme/UowemeApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UownmeApplication {
+public class UowemeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UownmeApplication.class, args);
+        SpringApplication.run(UowemeApplication.class, args);
     }
 }
--- src/test/java/com/dev/training/uownme/UowemeApplicationTests.java
+++ src/test/java/com/dev/training/uownme/UowemeApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class UownmeApplicationTests {
+public class UowemeApplicationTests {
 
     @Test
     public void contextLoads() {
